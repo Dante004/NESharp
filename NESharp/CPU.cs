238c0aeca407127f1b2f4acb91d0ef486ed80a96
Bartosz Bąk <bartosz.bk04@gmail.com>
2018-03-11 21:20:54 +0100

    Add very important comments

---
+++ NESharp/CPU.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//												NTSC		PAL
+//Frames per second								60			50
+//Time per frame (milliseconds)					16.67		20
+//Scanlines per frame (of which is V-Blank)		262 (20)	312 (70)
+//CPU cycles per scanline						113.33		106.56
+//Resolution									256 x 224	256 x 240
+//CPU speed										1.79 MHz	1.66 MHz
+namespace NESharp
+{
+    class CPU
+    {
+
+    }
+}
