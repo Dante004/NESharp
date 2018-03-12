f59e044562dc315858aeda13490119c33d1d66c9
Bartosz Bąk <bartosz.bk04@gmail.com>
2018-03-12 21:18:59 +0100

    add comment

--- NESharp/CPU.cs
+++ NESharp/CPU.cs
@@ -15,6 +15,14 @@ namespace NESharp
 {
     class CPU
     {
-
+        /// Processor Flag
+		/// 1) Carry flag        - Set if the last instruction resulted in an over or underflow. Used for arithmetic on numbers larger than one byte, where the next instruction is carry-flag aware.
+		/// 2) Zero flag         - Set if the last instruction resulted in a value of 0
+		/// 3) Interrupt Disable - Set to disable responding to maskable interrupts
+		/// 4) Decimal Mode      - Set to enable BCD mode. This doesn't affect the 2A03 so flipping this value doesn't do anything.
+		/// 5) Break Command     - Set to indicate a `BRK` instruction was executed
+		/// 6) Unused bit
+		/// 7) Overflow flag     - Set when an invalid two's complement number is the result of an operation. An example is adding 2 positive numbers which results in the sign bit being set, making the result a negative.
+		/// 8) Negative flag     - Set if the number is negative, determined by checking the sign bit (7th bit)
     }
 }
