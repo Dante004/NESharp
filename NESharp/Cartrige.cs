1adab58a68833320781b4075e03a866cf22be617
Dante <bartosz.bk04@gmail.com>
2018-03-18 13:58:07 +0100

    add new class cartidge

---
+++ NESharp/Cartrige.cs
@@ -0,0 +1,21 @@
+﻿using System.IO;
+
+namespace NESharp
+{
+    class Cartridge
+    {
+        const int headerMagic = 0x1A53454E;
+
+        byte[] pgrROM;
+        byte[] chr;
+        byte[] pgrRAM;
+    
+        public Cartridge(string path)
+        {
+            FileStream stream = new FileStream(path, FileMode.Open, FileAccess.Read);
+            BinaryReader reader = new BinaryReader(stream);
+
+            pgrRAM = new byte[8192];
+        }
+    }
+}
