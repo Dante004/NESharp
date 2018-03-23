304f38deb0ea32a8996526d82f9ad39b530dc665
Dante <bartosz.bk04@gmail.com>
2018-03-23 21:47:38 +0100

    Parse header in Cartridge

--- NESharp/Cartrige.cs
+++ NESharp/Cartrige.cs
@@ -1,21 +1,85 @@
 ﻿using System.IO;
+using System;
 
 namespace NESharp
 {
     class Cartridge
     {
-        const int headerMagic = 0x1A53454E;
-
+        public Console console;
+        const int HeaderMagic = 0x1A53454E;
         byte[] pgrROM;
-        byte[] chr;
+        byte[] chrROM;
         byte[] pgrRAM;
-    
-        public Cartridge(string path)
+        //How many PGR ROM has banks 16kB
+        int pgrRomBanks;
+        //How many CHR ROM has banks 8kB
+        int chrRomBanks;
+        bool useChrRAM;
+
+        int flag6;
+        int flag7;
+
+        int mapperNumber;
+
+        public Cartridge(string path,Console console)
         {
+            this.console = console;
             FileStream stream = new FileStream(path, FileMode.Open, FileAccess.Read);
             BinaryReader reader = new BinaryReader(stream);
+            ParseHeader(reader);
+        }
 
-            pgrRAM = new byte[8192];
+        void ParseHeader(BinaryReader reader)
+        {
+            uint magicNumber = reader.ReadUInt32();
+            if(HeaderMagic != magicNumber)
+            {
+                System.Console.WriteLine("Magic number is wrong");
+                return;
+            }
+
+            pgrRomBanks = reader.ReadByte();
+
+            chrRomBanks = reader.ReadByte();
+            //what if cartridge use chrc ram
+            if(chrRomBanks ==0)
+            {
+                chrRomBanks = 2;
+                useChrRAM = true;
+            }
+            else
+            {
+                useChrRAM = false;
+            }
+
+            // Flags 6
+            // 76543210
+            // ||||||||
+            // |||||||+-Mirroring: 0: horizontal(vertical arrangement)(CIRAM A10 = PPU A11)
+            // |||||||             1: vertical(horizontal arrangement)(CIRAM A10 = PPU A10)
+            // ||||||+--1: Cartridge contains battery - backed PRG RAM($6000 - 7FFF) or other persistent memory
+            // |||||+---1: 512 - byte trainer at $7000 -$71FF(stored before PRG data)
+            // ||||+----1: Ignore mirroring control or above mirroring bit; instead provide four - screen VRAM
+            // ++++---- - Lower nybble of mapper number
+            flag6 = reader.ReadByte();
+            //TODO: Finish flags 6
+
+            // Flags 7
+            // 76543210
+            // ||||||||
+            // |||||||+-VS Unisystem
+            // ||||||+--PlayChoice - 10(8KB of Hint Screen data stored after CHR data)
+            // ||||++---If equal to 2, flags 8 - 15 are in NES 2.0 format
+            // ++++---- - Upper nybble of mapper number
+            flag7 = reader.ReadByte();
+
+            mapperNumber = flag7 & 0xF0 | (flag6 >> 4 & 0xF);
+
+            if((flag7 & 0x0b0000_1100)== 0x0b0000_1100)
+            {
+                System.Console.WriteLine("This cartridge is in NES 2.0 format");
+            }
+
         }
     }
 }
