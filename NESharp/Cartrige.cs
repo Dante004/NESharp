064425e2517323f333411492b5b100ef48f42914
Dante <bartosz.bk04@gmail.com>
2018-03-24 17:44:10 +0100

    Complete Cartridge class

--- NESharp/Cartrige.cs
+++ NESharp/Cartrige.cs
@@ -5,7 +5,6 @@ namespace NESharp
 {
     class Cartridge
     {
-        public Console console;
         const int HeaderMagic = 0x1A53454E;
         byte[] pgrROM;
         byte[] chrROM;
@@ -19,14 +18,21 @@ namespace NESharp
         int flag6;
         int flag7;
 
+        public bool verticalVRAMMirroring;
+        //If true cartridge contains a 512 byte trainer
+        public bool containsTrainers;
+
         int mapperNumber;
 
-        public Cartridge(string path,Console console)
+        public Cartridge(string path)
         {
-            this.console = console;
             FileStream stream = new FileStream(path, FileMode.Open, FileAccess.Read);
             BinaryReader reader = new BinaryReader(stream);
             ParseHeader(reader);
+            LoadPGRROM(reader);
+            LoadCHRROM(reader);
+
+            pgrRAM = new byte[8192];
         }
 
         void ParseHeader(BinaryReader reader)
@@ -62,6 +68,9 @@ namespace NESharp
             // ||||+----1: Ignore mirroring control or above mirroring bit; instead provide four - screen VRAM
             // ++++---- - Lower nybble of mapper number
             flag6 = reader.ReadByte();
+            verticalVRAMMirroring = (flag6 & 0x01) != 0;
+            containsTrainers = (flag6 & 0x04) != 0;
+
             //TODO: Finish flags 6
 
             // Flags 7
@@ -80,6 +89,52 @@ namespace NESharp
                 System.Console.WriteLine("This cartridge is in NES 2.0 format");
             }
 
+        }
+
+        void LoadPGRROM(BinaryReader reader)
+        {
+            int pgrRomOffset = containsTrainers ? 16 + 512 : 16;
+            reader.BaseStream.Seek(pgrRomOffset, SeekOrigin.Begin);
+            pgrROM = new byte[pgrRomBanks * 16383];
+            reader.Read(pgrROM, 0, pgrRomBanks * 16383);
+        }
+        void LoadCHRROM(BinaryReader reader)
+        {
+            if(useChrRAM)
+            {
+                chrROM = new byte[8192];
+            }
+            else
+            {
+                chrROM = new byte[chrRomBanks * 8192];
+                reader.Read(chrROM, 0, chrRomBanks * 8192);
+            }
+        }
+
+        public byte ReadPGRROM(int index)
+        {
+            return pgrROM[index];
+        }
+
+        public byte ReadPGRRAM(int index)
+        {
+            return pgrRAM[index];
+        }
+
+        public void WritePGRRAM(int index,byte valume)
+        {
+            pgrRAM[index] = valume;
+        }
+
+        public byte ReadCHR(int index)
+        {
+            return chrROM[index];
+        }
+
+        public void WriteCHR(int index,byte valume)
+        {
+            if (useChrRAM) return;
+            chrROM[index] = valume;
         }
     }
 }
